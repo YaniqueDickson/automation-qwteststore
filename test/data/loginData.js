fa75390d20dfd54fbc2e6e08ea4344b6dab56380
YaniqueDickson <ydickson@qualityworkscg.com>
2020-11-16 19:09:18 -0500

    Data file added with valid and invalid login credentials

---
+++ test/data/loginData.js
@@ -0,0 +1,8 @@
+module.exports = {
+    validEmail: 'ydickson@qualityworkscg.com',
+    validPassword: 'Password!',
+    invalidEmail: 'Yanique@yes.com',
+    invalidPassword: 'notpassword',
+    // validEmail: 'neikidix@gmail.com',
+    // validPassword: 'Password',
+}
